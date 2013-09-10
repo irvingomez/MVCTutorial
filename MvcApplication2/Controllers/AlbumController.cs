99efcbea25b105b7680847d64bbc0c34064e3282
Irvin Gomez <irvin.gomez@sieena.com>
2013-09-09 23:34:36 -0500

    completed webapi examples

---
+++ MvcApplication2/Controllers/AlbumController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MvcApplication2.Models;
+using MvcApplication2.Repositories;
+
+namespace MvcApplication2.Controllers
+{
+    public class AlbumController : ApiController
+    {
+        // GET api/album
+        public IEnumerable<Album> Get()
+        {
+            var albums = AlbumRepository.FindAll();
+            return albums;
+        }
+
+        // GET api/album/5
+        public Album Get(int id)
+        {
+            var album = AlbumRepository.FindAll().Where(x => x.Id == id).FirstOrDefault();
+            return album;
+        }
+
+        // POST api/album
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/album/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/album/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MvcApplication2/Models/AlbumModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Artist { get; set; }
+    }
+}
